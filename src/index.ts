5b60dcc34d0e8067f9fc01e20f27d89b6a638fe0
Will FP <william.favierparsons1@gmail.com>
2025-02-20 18:40:35 +0000

    Updated structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export * from "./rapidcron";
+export { default } from './rapidcron';
+// Also export any other types or utilities you want to make public
+export * from './types/tasks';
+export * from './types/requests';
+
